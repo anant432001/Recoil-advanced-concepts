f151550c27f47180a3acf6012db0d6a31eb9d758
Anant Pandey <anant432001@gmail.com>
2024-06-17 12:09:52 +0530

    fc

--- 7.4-async-queries/src/atoms.js
+++ 7.4-async-queries/src/atoms.js
@@ -1,22 +1,26 @@
+import axios from "axios";
 import { atom, selector } from "recoil";
 
 export const notifications = atom({
-    key: "networkAtom",
-    default: {
-        network: 4, 
-        jobs: 6, 
-        messaging: 3, 
-        notifications: 3
-    }
+  key: "networkAtom",
+  default: selector({
+    key: "notificationsSelector",
+    get: async () => {
+      const res = await axios.get("your link");
+      return res.data;
+    },
+  }),
 });
 
 export const totalNotificationSelector = selector({
-    key: "totalNotificationSelector",
-    get: ({get}) => {
-        const allNotifications = get(notifications);
-        return allNotifications.network + 
-        allNotifications.jobs + 
-        allNotifications.notifications + 
-        allNotifications.messaging
-    }
-})
+  key: "totalNotificationSelector",
+  get: ({ get }) => {
+    const allNotifications = get(notifications);
+    return (
+      allNotifications.network +
+      allNotifications.jobs +
+      allNotifications.notifications +
+      allNotifications.messaging
+    );
+  },
+});
